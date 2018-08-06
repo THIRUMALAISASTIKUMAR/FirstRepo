e0ecb1d9e6da1ec797e080fefb494d91de4cfbd9
THIRUMALAISASTIKUMAR <42150637+THIRUMALAISASTIKUMAR@users.noreply.github.com>
2018-08-07 00:03:20 +0530

    cmt line

--- JSONDemo.java
+++ JSONDemo.java
@@ -7,7 +7,7 @@ public class JSONDemo {
 	public static void main(String[] args) {
 
 		JSONObject obj = new JSONObject();
-		obj.put("Thiru", 50);
+		//obj.put("Thiru", 50);
 		obj.put("Vihesh", 60);
 		obj.put("Selvi", 40);
 		obj.put("Sasti", 40);
