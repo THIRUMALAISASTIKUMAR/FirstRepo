16835364643dab22123232913be694e6456ce926
THIRUMALAISASTIKUMAR <42150637+THIRUMALAISASTIKUMAR@users.noreply.github.com>
2018-08-06 23:10:22 +0530

    JSON Example File Added

---
+++ JSONDemo.java
@@ -0,0 +1,15 @@
+package com.json;
+
+import org.json.simple.JSONObject;
+
+public class JSONDemo {
+
+	public static void main(String[] args) {
+
+		JSONObject obj = new JSONObject();
+		obj.put("Thiru", 50);
+		obj.put("Vihesh", 60);
+		System.out.println(obj);
+	}
+
+}
