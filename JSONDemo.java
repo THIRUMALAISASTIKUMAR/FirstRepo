4fcec743551fa8dea2f8f96c346160c8b9bd84fc
THIRUMALAISASTIKUMAR <42150637+THIRUMALAISASTIKUMAR@users.noreply.github.com>
2018-08-08 14:46:47 +0530

    Updated on 08Aug2018

--- JSONDemo.java
+++ JSONDemo.java
@@ -7,9 +7,8 @@ public class JSONDemo {
 	public static void main(String[] args) {
 
 		JSONObject obj = new JSONObject();
-		//obj.put("Thiru", 50);
+		obj.put("Thiru", 50);
 		obj.put("Vihesh", 60);
-		obj.put("Selvi", 40);
 		obj.put("Sasti", 40);
 		System.out.println(obj);
 	}
